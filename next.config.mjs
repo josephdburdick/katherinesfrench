2685e24502dc27f3d6e4421543c922969319e509
Joe <josephdburdick@gmail.com>
2025-01-30 11:10:10 -0500

    Update basePath for production

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProduction = process.env.NODE_ENV === "production"
 
 const nextConfig = {
-  basePath: isProduction ? "/katherinesfrench" : "",
+  basePath: isProduction ? "" : "/katherinesfrench",
   output: "export",
   images: {
     unoptimized: true,
